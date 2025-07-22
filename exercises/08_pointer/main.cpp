268226f800de46805fdc945c0246fe1e4d78029d
jasonkayzk <jasonkayzk@gmail.com>
2025-07-22 14:34:41 +0800

    finished: 0-9

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -5,6 +5,22 @@ bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
     // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
     // arr[i + 2] = arr[i] + arr[i + 1]
+    for (int i = 0; i <= len - 3; ++i) {
+        // 计算下一个可能的索引
+        int idx1 = i * stride;
+        int idx2 = (i + 1) * stride;
+        int idx3 = (i + 2) * stride;
+
+        // 检查索引是否越界
+        if (idx1 >= len * stride || idx2 >= len * stride || idx3 >= len * stride) {
+            return false;
+        }
+
+        if (ptr[idx1] + ptr[idx2] != ptr[idx3]) {
+            return false;
+        }
+    }
+
     return true;
 }
 
