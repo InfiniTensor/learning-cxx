4b5a32fee26f59385a645de4726fa90ab895e2b5
lcollection <1305787491@qq.com>
2025-08-16 21:01:48 +0800

    123

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for(int i=0;i<4;i++){
+            shape[i]=shape_[i];
+            size*=shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,23 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        unsigned int size = 1;
+        unsigned int idx[4] = {};
+        for (int i = 0; i < 4; ++i) size *= shape[i];
+        for (unsigned int i = 0; i < size; ++i) {
+            unsigned int remain = i;
+            for(int d = 3; d >= 0; --d) {
+                idx[d] = remain % shape[d];
+                remain /= shape[d];
+            }
+            unsigned int o_idx = 0, o_stride = 1;
+            for (int d = 3; d >= 0; --d) {
+                unsigned int pos = (others.shape[d] == 1) ? 0 : idx[d];
+                o_idx += pos * o_stride;
+                o_stride *= others.shape[d];
+            }
+            data[i] += others.data[o_idx];
+        }
         return *this;
     }
 };
