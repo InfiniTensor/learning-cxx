4de67296aa1c978d34a539f86d8dd467cc7754e8
WHoutstanding <wh15839417029@outlook.com>
2025-12-29 12:03:39 +0800

    finished

--- exercises/23_template_const/main.cpp
+++ exercises/23_template_const/main.cpp
@@ -1,5 +1,5 @@
 ﻿#include "../exercise.h"
-#include <cstring>
+#include <array>
 
 // READ: 模板非类型实参 <https://zh.cppreference.com/w/cpp/language/template_parameters#%E6%A8%A1%E6%9D%BF%E9%9D%9E%E7%B1%BB%E5%9E%8B%E5%AE%9E%E5%8F%82>
 
@@ -11,8 +11,11 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
-        data = new T[size];
-        std::memset(data, 0, size * sizeof(T));
+        for (decltype(N) i = 0; i < N; i++) {
+            size *= shape_[i];
+            shape[i] = shape_[i];
+        }
+        data = new T[size]();
     }
     ~Tensor() {
         delete[] data;
@@ -32,11 +35,20 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
+        auto strides = this->strides();
         for (unsigned int i = 0; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
+            index += strides[i] * indices[i];
         }
         return index;
+    }
+
+    std::array<unsigned int, N> strides() const {
+        std::array<unsigned int, N> strides;
+        strides[N - 1] = 1;
+        for (int i = N - 2; i >= 0; i--) { strides[i] = strides[i + 1] * shape[i + 1]; }
+        return strides;
     }
 };
 
