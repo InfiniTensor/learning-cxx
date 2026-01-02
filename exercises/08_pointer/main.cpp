c44bae76ca47cfe49d5072eef9d95f22f8ab30ac
Squarefang1217 <squarew85@gmail.com>
2026-01-02 20:18:40 +0800

    完成所有C++基础练习

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -5,6 +5,20 @@ bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
     // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
     // arr[i + 2] = arr[i] + arr[i + 1]
+    for (int i = 0; i < len - 2; ++i) {
+        // 计算逻辑索引对应的物理内存值
+        // 第 1 个数：ptr[i * stride]
+        // 第 2 个数：ptr[(i + 1) * stride]
+        // 第 3 个数：ptr[(i + 2) * stride]
+        int a = ptr[i * stride];
+        int b = ptr[(i + 1) * stride];
+        int c = ptr[(i + 2) * stride];
+
+        // 如果发现任意一组不满足斐波那契规则，直接返回 false
+        if (c != a + b) {
+            return false;
+        }
+    }
     return true;
 }
 
