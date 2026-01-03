e3a116b0d28c203636cf429946db0d31bd526bc4
Dnoob <568131551@example.com>
2026-01-03 21:34:08 +0800

    完成 exercises 0-33

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4; i++) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,33 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+
+        unsigned int o_strides[4];
+        o_strides[3] = 1;
+        o_strides[2] = others.shape[3];
+        o_strides[1] = others.shape[2] * others.shape[3];
+        o_strides[0] = others.shape[1] * others.shape[2] * others.shape[3];
+
+        T* current_ptr = this->data;
+        for (unsigned int n = 0; n < shape[0]; ++n) {
+            unsigned int n_off = (n % others.shape[0]) * o_strides[0];
+
+            for (unsigned int c = 0; c < shape[1]; ++c) {
+                unsigned int c_off = (c % others.shape[1]) * o_strides[1];
+
+                for (unsigned int h = 0; h < shape[2]; ++h) {
+                    unsigned int h_off = (h % others.shape[2]) * o_strides[2];
+                    
+                    for (unsigned int w = 0; w < shape[3]; ++w) {
+                        unsigned int w_off = (w % others.shape[3]) * o_strides[3];
+                        unsigned int other_idx = n_off + c_off + h_off + w_off;
+
+                        *current_ptr += others.data[other_idx];
+                        current_ptr++;
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
